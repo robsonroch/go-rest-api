8f866f03488e972e0359b67e43c4e46bc423d9d2
robsonroch <robson.proxycode@gmail.com>
2026-01-04 18:00:02 -0300

    testing service layers

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -5,7 +5,17 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type repositoryMock struct {
+	mock.Mock
+}
+
+func (r *repositoryMock) Save(campaign *Campaign) error {
+	args := r.Called(campaign)
+	return args.Error(0)
+}
 
 func Test_Create_Campaign(t *testing.T) {
 	assert := assert.New(t)
@@ -23,7 +33,9 @@ func Test_Create_Campaign(t *testing.T) {
 
 func Test_Create_SaveCampaign(t *testing.T) {
 	assert := assert.New(t)
-	service := Service{}
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.AnythingOfType("*campaign.Campaign")).Return(nil)
+	service := Service{Repository: repositoryMock}
 	newCampaign := contract.NewCampaign{
 		Name:    "Test Y",
 		Content: "Body",
